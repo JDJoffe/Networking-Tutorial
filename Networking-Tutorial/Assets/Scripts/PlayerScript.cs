0429591a5335f4b8e7994009f445b77568d79e9d
josh <48034855+JDJoffe@users.noreply.github.com>
2019-07-31 12:04:25 +1000

    initial

---
+++ Networking-Tutorial/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerScript : NetworkBehaviour
+{
+    [Header("Variables")]
+    public bool _isGrounded = false;
+    public float _moveSpeed = 10f, _rotationSpeed = 5f, _jumpSpeed = 2f;
+    private Rigidbody _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+
+        AudioListener _audioListener = GetComponentInChildren<AudioListener>();
+        Camera _camera = GetComponentInChildren<Camera>();
+
+        //if player is not local disable camera+sounds
+        if (isLocalPlayer)
+        {
+            _camera.enabled = true;
+            _audioListener.enabled = true;
+        }
+        else
+        {
+            _camera.enabled = false;
+            _audioListener.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //can only move localplayer
+        if (isLocalPlayer)
+        {
+            HandleInput();
+        }
+    }
+
+    public void Move(KeyCode _key)
+    {
+        Vector3 _pos = _rigid.position;
+        Quaternion _rotation = _rigid.rotation;
+
+        //Movement direction
+        switch (_key)
+        {
+            case KeyCode.W:
+                _pos += transform.forward * _moveSpeed * Time.deltaTime;
+                break;
+            case KeyCode.A:
+                _rotation *= Quaternion.AngleAxis(-_rotationSpeed, Vector3.up);
+                break;
+            case KeyCode.S:
+                _pos -= transform.forward * _moveSpeed * Time.deltaTime;
+                break;
+            case KeyCode.D:
+                _rotation *= Quaternion.AngleAxis(_rotationSpeed, Vector3.up);
+                break;
+            case KeyCode.Space:
+                if (_isGrounded)
+                {
+                    _rigid.AddForce(Vector3.up * _jumpSpeed, ForceMode.Impulse);
+                    _isGrounded = false;
+                }
+                break;
+        }
+        //IMPORTANT
+        //actually move
+        _rigid.MovePosition(_pos);
+        _rigid.MoveRotation(_rotation);
+    }
+    void OnCollisionEnter(Collision _col)
+    {
+        _isGrounded = true;
+    }
+    void HandleInput()
+    {
+        //GetKeys
+        KeyCode[] keys =
+        {
+            KeyCode.W,
+            KeyCode.A,
+            KeyCode.S,
+            KeyCode.D,
+            KeyCode.Space
+        };
+        foreach (var _key in keys)
+        {
+            if (Input.GetKey(_key))
+            {
+                Move(_key);
+            }
+        }
+    }
+}
---
+++ Networking-Tutorial/Assets/Scripts/SyncTransform.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncTransform : NetworkBehaviour
+{
+    [Header("Variables")]
+    //speed of lerping rotation&position
+    public float _lerpRate = 15;
+
+    //command send threshhold
+    public float _positionThreshold = 0.5f;
+    public float _rotationThreshold = 5.0f;
+    //record prev rotation and position sent to the server
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+    //vars synced across network
+    [SyncVar] private Vector3 _syncPosition;
+    [SyncVar] private Quaternion _syncRotation;
+
+    //get rigidbody 
+    private Rigidbody _rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        TransmitPosition();
+        LerpPosition();
+
+        TransmitRotation();
+        LerpRotation();
+    }
+    #region Lerps
+    void LerpPosition()
+    {
+        //if not the local player
+        if (!isLocalPlayer)
+        {
+            //Lerp position of other connected clients
+            _rigid.position = Vector3.Lerp(_rigid.position, _syncPosition, Time.deltaTime * _lerpRate);
+        }
+    }
+    void LerpRotation()
+    {
+        //if not local player
+        if (!isLocalPlayer)
+        {
+            //lerp rotation of other connected clients
+            _rigid.rotation = Quaternion.Lerp(_rigid.rotation, _syncRotation, Time.deltaTime * _lerpRate);
+        }
+    }
+    #endregion
+    #region Serverstuff
+    [Command]
+    void CmdSendPositionToServer(Vector3 _position)
+    {
+        _syncPosition = _position;
+        Debug.Log("Position Command");
+    }
+    [Command]
+    void CmdSendRotationToServer(Quaternion _rotation)
+    {
+        _syncRotation = _rotation;
+        Debug.Log("Rotation Command");
+    }
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if (isLocalPlayer)
+        {
+            CmdSendPositionToServer(_rigid.position);
+        }
+    }
+    [ClientCallback]
+    void TransmitRotation()
+    {
+        if (isLocalPlayer)
+        {
+            CmdSendRotationToServer(_rigid.rotation);
+        }
+    }
+    #endregion
+}
