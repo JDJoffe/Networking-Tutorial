b699dde867e59827e426f87e3d25eab65b327500
josh <48034855+JDJoffe@users.noreply.github.com>
2019-07-31 15:50:49 +1000

    saving

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Xml;
+using System.Xml.Serialization;
+[XmlRoot("Game Data Collection")]
+public class GameData
+{
+    public Vector3 _position;
+    public Quaternion _rotation;
+    [XmlArray("Dialogue")]
+    [XmlArrayItem("Text")]
+    public string[] dialogue;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform _player;
+    private GameData data = new GameData();
+    public string fileName = "GameData.xml";
+
+
+    public void Load(string path)
+    {
+       
+    }
+    public void Save(string path)
+    {
+        var serializer = new XmlSerializer(typeof( GameData));
+        var stream = new FileStream(path, FileMode.Create);
+        serializer.Serialize(stream, data);
+        stream.Close();
+        Debug.Log("File Saved Successfully to " + path);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = Application.dataPath +"/"+ fileName;
+        Load(path);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            _player = FindObjectOfType<PlayerScript>().transform;
+            data._position = _player.position;
+            data._rotation = _player.rotation;
+            data.dialogue = new string[]
+            {
+                "Hello",
+                "World"
+            };
+            Save(Application.dataPath +"/"+ fileName);
+        }
+    }
+
+}
